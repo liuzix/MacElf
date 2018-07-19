6ed9b6dfb6079bdc0cba54c722f2bb23bac02e16
李云霞 <zixiongliu@zixiongs-macbook-pro.local>
2018-07-19 02:30:15 -0400

    Initial Commit

---
+++ MacElf/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MacElf
+//
+//  Created by Zixiong Liu on 7/19/18.
+//  Copyright © 2018 Zixiong Liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
